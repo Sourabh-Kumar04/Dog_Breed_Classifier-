2e8ddc89a669429ea1dcd85305db4160a63f139d
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2024-07-14 17:53:27 +0530

    add README.md, license, requirement.txt

--- data/check_images.py
+++ data/check_images.py
@@ -39,59 +39,36 @@ from print_results import print_results
 
 # Main program function defined below
 def main():
-    # Sets start time
     start_time = time()
-    
-    # Defines get_input_args function within the file get_input_args.py
     in_arg = get_input_args()
 
-    # Checks command line arguments using in_arg  
     check_command_line_arguments(in_arg)
 
-    # Creates the results dictionary that contains the results, 
-    # this dictionary is returned from the function call as the variable results
     results = get_pet_labels(in_arg.dir)
-
-    # Checks Pet Images in the results Dictionary using results    
+   
     check_creating_pet_image_labels(results)
     
-    # Creates Classifier Labels with classifier function, Compares Labels, 
-    # and adds these results to the results dictionary - results
     classify_images(in_arg.dir, results, in_arg.arch)
 
-    # Function that checks Results Dictionary using results    
     check_classifying_images(results)    
     
-    # Adjusts the results dictionary to determine if classifier correctly 
-    # classified images as 'a dog' or 'not a dog'. This demonstrates if 
-    # model can correctly classify dog images as dogs (regardless of breed)
     adjust_results4_isadog(results, in_arg.dogfile)
 
-    # Checks Results Dictionary for is-a-dog adjustment using results
     check_classifying_labels_as_dogs(results)
 
-    # Calculates results of run and puts statistics in the Results Statistics
-    # Dictionary - called results_stats
     results_stats = calculates_results_stats(results)
 
-    # Function that checks Results Statistics Dictionary using results_stats
     check_calculating_results(results, results_stats)
 
-    # Prints summary results, incorrect classifications of dogs (if requested)
-    # and incorrectly classified breeds (if requested)
     print_results(results, results_stats, in_arg.arch, True, True)
     
-    # Check time to code
     end_time = time()
     
-    # Computes overall runtime in seconds & prints it in hh:mm:ss format
-    # Calculate difference between end time and start time
     tot_time = end_time - start_time 
     print("\n** Total Elapsed Runtime:",
           str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
           +str(round((tot_time%3600)%60)) )
     
 
-# Call to main function to run the program
 if __name__ == "__main__":
     main()
