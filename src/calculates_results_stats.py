cf256afa5f71bdcc0a6712ab805cbb59d339060b
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2024-07-17 19:33:33 +0530

    #TODO 5

--- src/calculates_results_stats.py
+++ src/calculates_results_stats.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
 #                                                                             
-# PROGRAMMER:
-# DATE CREATED:                                  
+# PROGRAMMER: Sourabh Kumar (Sourabh-Kumar04)
+# DATE CREATED: 17 July, 2024                                 
 # REVISED DATE: 
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
@@ -67,7 +67,75 @@ def calculates_results_stats(results_dic):
                      and the value is the statistic's value. See comments above
                      and the previous topic Calculating Results in the class for details
                      on how to calculate the counts and statistics.
-    """        
+    """   
+     
+    # Creates empty dictionary for results_stats
+    results_stats = {}
+    
+    # Sets all counters to initial values of zero so that they can 
+    # be incremented while processing through the images in results_dic 
+    results_stats['n_dogs_img'] = 0
+    results_stats['n_match'] = 0
+    results_stats['n_correct_dogs'] = 0
+    results_stats['n_correct_notdogs'] = 0
+    results_stats['n_correct_breed'] = 0       
+    
+    # Process through the results dictionary
+    for key in results_dic:
+        # Labels Match Exactly
+        if results_dic[key][2] == 1:
+            results_stats['n_match'] += 1
+            
+        # Pet Image Label is a Dog AND Labels match- counts Correct Breed
+        if sum(results_dic[key][2:]) == 3:
+            results_stats['n_correct_breed'] += 1
+        
+        # Pet Image Label is a Dog - counts number of dog images
+        if results_dic[key][3] == 1:
+            results_stats['n_dogs_img'] += 1
+            
+            # Classifier classifies image as Dog (& pet image is a dog)
+            # counts number of correct dog classifications
+            if results_dic[key][4] == 1:
+                results_stats['n_correct_dogs'] += 1
+                
+        # Pet Image Label is NOT a Dog
+        else:
+            # Classifier classifies image as NOT a Dog(& pet image isn't a dog)
+            # counts number of correct NOT dog clasifications.
+            if results_dic[key][4] == 0:
+                results_stats['n_correct_notdogs'] += 1
+
+    # Calculates run statistics (counts & percentages) below that are calculated
+    # using counters from above.
+    
+    # Calculates number of total images
+    results_stats['n_images'] = len(results_dic)
+
+    # Calculates number of not-a-dog images using - images & dog images counts
+    results_stats['n_notdogs_img'] = (results_stats['n_images'] - 
+                                      results_stats['n_dogs_img']) 
+
+    # Calculates % correct for matches
+    results_stats['pct_match'] = (results_stats['n_match'] / 
+                                  results_stats['n_images']) * 100.0
+    
+    # Calculates % correct dogs
+    results_stats['pct_correct_dogs'] = (results_stats['n_correct_dogs'] / 
+                                         results_stats['n_dogs_img']) * 100.0    
+
+    # Calculates % correct breed of dog
+    results_stats['pct_correct_breed'] = (results_stats['n_correct_breed'] / 
+                                          results_stats['n_dogs_img']) * 100.0
+
+    # Calculates % correct not-a-dog images
+    # Uses conditional statement for when no 'not a dog' images were submitted 
+    if results_stats['n_notdogs_img'] > 0:
+        results_stats['pct_correct_notdogs'] = (results_stats['n_correct_notdogs'] /
+                                                results_stats['n_notdogs_img']) * 100.0
+    else:
+        results_stats['pct_correct_notdogs'] = 0.0
+
     # Replace None with the results_stats_dic dictionary that you created with 
     # this function 
-    return None
+    return results_stats
